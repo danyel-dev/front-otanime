67a37ab1a3ca88bb3dcf6ef38de2ad6c32ce8034
danyel-dev <dannykill4221x@gmail.com>
2024-03-11 12:20:41 -0300

    a

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,48 +6,28 @@ import Header from "./Components/Header";
 
 
 export default function Home() {
-  const [currentSeasonAnimes, setCurrentSeasonAnimes] = useState([])
-  const [currentSeasonName, setCurrentSeasonName] = useState("")
-  const [search, setSearch] = useState("")
-  const [pagination, setPagination] = useState([]);
-  
+  const [seasonAnimes, setSeasonAnimes] = useState([])
+  const [pagination, setPagination] = useState([])
+
   const params = new URLSearchParams(window.location.search);
   const page = params.get('page');
-
+ 
   useEffect(() => {
-    let url;
-
-    if(page) {
-      url = `https://api.jikan.moe/v4/seasons/now?page=${page}` 
-    } else {
-      url = 'https://api.jikan.moe/v4/seasons/now'
-    }
+    let url = 'https://api.jikan.moe/v4/seasons/now'
+    
+    if(page)
+      url = url + `?page=${page}`
 
     axios.get(url).then(response => 
     {
-      setCurrentSeasonAnimes(response.data.data)
-      setCurrentSeasonName(response.data.data[0].season)
-      console.log(response.data.pagination)
+      setSeasonAnimes(response.data.data)
+      console.log(response.data)
 
       const novoVetor = Array.from({ length: response.data.pagination.last_visible_page }, (_, index) => index + 1);
       setPagination(novoVetor);
     })
   }, [page])
   
-  function handleChangeSearchValue(e) {
-    setSearch(e.target.value)
-  }
-
-  function handleSubmitFormSearch(e) {
-    e.preventDefault()
-
-    axios.get(`https://api.jikan.moe/v4/anime?q=${search}`).then(response => 
-    {
-      setCurrentSeasonAnimes(response.data.data)
-      console.log(response.data)
-    })    
-  }
-
   return (
     <div className={styles.app}>
       <Header />
@@ -58,31 +38,25 @@ export default function Home() {
             Aqui no otanime você encontra um enorme acervo de animes, pesquise por um anime do seu interrese e veja mais informações.
           </h1>
 
-          <form className={styles.formSearchAnime} onSubmit={handleSubmitFormSearch}>
+          <form className={styles.formSearchAnime}>
             <i className="fa-solid fa-magnifying-glass"></i>
-            <input type="text" placeholder="Pesquise um anime aqui" value={search} onChange={handleChangeSearchValue} />
+            <input type="text" placeholder="Pesquise um anime aqui" />
           </form>
         </div>
 
         <div className={styles.listAnimes}>
-          <h1>Animes da temporada Atual ({currentSeasonName})</h1>
+          <h1>Animes da temporada Atual</h1>
 
-          <div className={styles.currentSeasonAnimes}>
-            {currentSeasonAnimes.map(anime => <Anime key={anime.mal_id} anime={anime} />)}
+          <div className={styles.seasonAnimes}>
+            {seasonAnimes.map(anime => <Anime key={anime.mal_id} anime={anime} />)}
           </div>
-
-          <nav className={styles.pagination}>
-            <ul>
-              {
-                pagination.map(page => {
-                  return(
-                    <li><a href={`?page=${page}`}>{page}</a></li>
-                  )
-                })
-              }
-            </ul>
-          </nav>
         </div>
+
+        <nav className={styles.pagination}>
+          <ul>
+            {pagination.map(page => <li><a href={`/?page=${page}`}>{page}</a></li> )}
+          </ul>
+        </nav>
       </main>
     </div>
   );
